82d7143b890552b23cc194c3f9b9616ff822ed37
Matteo Sandrin <matteo.sandrin@hotmail.it>
2021-04-06 17:59:01 -0400

    Add hover opacity

--- js/index.js
+++ js/index.js
@@ -6,6 +6,16 @@ d3.csv("data/top_100_streamers_last_365_days.csv")
             .enter()
                 .append('div')
                     .attr('class', 'icon')
+                    .on('mouseover', function (d,i) {
+                        d3.select(this).transition()
+                            .duration(50)
+                            .style("opacity", "0.5");
+                    })
+                    .on('mouseout', function (d,i) {
+                        d3.select(this).transition()
+                            .duration(50)
+                            .style("opacity", "1.0");
+                    })
                 .append('img')
                     .attr('src', d => "img/" + d["Rank"] + "_"+ d["Channel URL"] + ".jpeg");
     }
