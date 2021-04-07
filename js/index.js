795affb986c179a0653514a8d92a877bc822856c
Matteo Sandrin <matteo.sandrin@hotmail.it>
2021-04-07 15:43:24 -0400

    Add streamer detail panel

--- js/index.js
+++ js/index.js
@@ -1,22 +1,62 @@
+let currStreamer = {};
 d3.csv("data/top_100_streamers_last_365_days.csv")
     .then(function (data) {
+        currStreamer = data[0];
+        setStreamerDetail(currStreamer);
         d3.select('#streamer-icons')
             .selectAll('div')
             .data(data)
             .enter()
                 .append('div')
                     .attr('class', 'icon')
-                    .on('mouseover', function (d,i) {
-                        d3.select(this).transition()
-                            .duration(50)
-                            .style("opacity", "0.5");
+                    .on('mouseover', function () {
+                        d3.select(this)
+                            .each(function (d) {setStreamerDetail(d)})
+                            .transition()
+                                .duration(50)
+                                .style("opacity", "0.5");
                     })
-                    .on('mouseout', function (d,i) {
-                        d3.select(this).transition()
-                            .duration(50)
-                            .style("opacity", "1.0");
+                    .on('mouseout', function () {
+                        d3.select(this)
+                            .each(function (d) {setStreamerDetail(currStreamer)})
+                            .transition()
+                                .duration(50)
+                                .style("opacity", "1.0");
+                    })
+                    .on('click', function() {
+                        d3.select(this)
+                            .each(function (d) {
+                                currStreamer = d;
+                            })
                     })
                 .append('img')
-                    .attr('src', d => "img/" + d["Rank"] + "_"+ d["Channel URL"] + ".jpeg");
+                    .attr('src', d => getImageUrl(d));
     }
 );
+
+function getImageUrl(streamer) {
+    return "img/" + streamer["Rank"] + "_"+ streamer["Channel URL"] + ".jpeg";
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function setStreamerDetail(streamer) {
+    d3.select('.icon-large > img').attr('src', getImageUrl(streamer));
+
+    d3.select('.streamer-title').text(streamer.Channel);
+
+    d3.select('#streamer-rank > .label').text('rank');
+    d3.select('#streamer-rank > .value').text(streamer.Rank);
+
+    d3.select('#streamer-followers > .label').text('followers');
+    d3.select('#streamer-followers > .value').text(numberWithCommas(streamer.Followers));
+
+    d3.select('#streamer-watch-time > .label').text('watch time');
+    const watch_hours = Math.round(streamer["Watch time"] / 60);
+    d3.select('#streamer-watch-time > .value').text(numberWithCommas(watch_hours) + " hours");
+
+    d3.select('#streamer-language > .label').text('language');
+    d3.select('#streamer-language > .value').text(streamer.Language);
+}
