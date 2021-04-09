0347eb1a23776f9fd3d1f5ace8d750f92811fa3d
Alex Cohen <acc2222@columbia.edu>
2021-04-08 22:06:30 -0400

    Added gantt data, rough chart, and selector

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let currStreamer = {};
-d3.csv("data/top_100_streamers_last_365_days.csv")
+d3.csv("https://raw.githubusercontent.com/matteosandrin/coms4995-data-vis-final-project/gantt-chart/data/top_100_streamers_last_365_days.csv")
     .then(function (data) {
         currStreamer = data[0];
         setStreamerDetail(currStreamer);
@@ -34,6 +34,117 @@ d3.csv("data/top_100_streamers_last_365_days.csv")
                     .attr('src', d => getImageUrl(d));
     }
 );
+
+
+// Gantt Chart
+d3.csv("https://raw.githubusercontent.com/AlexCCohen/data_viz_A5/main/avg_viewers_month_v3.csv")
+    .then(function (raw_data) {
+        data = getStreamsData(raw_data);
+
+        const date_extent = d3.extent(data.map(d => d.start));
+        const total_months = dateDiff(date_extent[0], date_extent[1]);
+        const text_padding = 20;
+        const date_scale = d3.scaleLinear()
+            .domain([0, total_months])
+            .range([text_padding, screen.width]);
+
+        const height = 1200;
+            
+            
+        const width_padding = 1;
+        const height_padding = 2;
+        const num_streamers = 100;
+        const rect_height = height/(num_streamers+1) - height_padding;
+        
+        const rect_width = (screen.width - text_padding)/total_months - width_padding;
+            
+        const color = d3.scaleSequential(d3.interpolateBlues).domain([-2000, 20000]);
+
+        d3.select('#gantt-chart-svg')
+            .attr("viewBox", [0, 0, screen.width, height])
+            .selectAll('rect')
+            .data(data)
+            .join('rect')
+                .attr("width", rect_width)
+                .attr("height", rect_height)
+                .attr("y", d => (rect_height + height_padding)*d.rank)
+                .attr("x", d => date_scale(dateDiff(d.start, date_extent[0])))
+                .attr("fill", d => color(d.avg_viewers))
+                .attr("opacity", 1)
+                .on("mouseover", function() {
+                    d3.select(this)
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 3);
+                    this.parentElement.appendChild(this);
+                })
+                .on("mouseout", function() {
+                    d3.select(this)
+                        .attr("stroke-width", 0);
+                });
+
+        d3.select("#gantt-chart-svg")
+            .append("g")
+            .selectAll("text")
+            .data([...Array(100).keys()].map(d=>d+1))
+            .join("text")
+                .attr("id", d => "streamer_text_" + d)
+                .attr("y", d => (rect_height + height_padding)*d + rect_height/2)
+                .attr("x", 5)
+                .text(d => d)
+                .attr("text-anchor", "start")
+                .attr("font-size", 9)
+                .on("mouseover", function(event, selectedData) {
+                    d3.select(this)
+                        .transition()
+                        .attr("font-size", 14);
+
+                    d3.select("#streamer_text_" + (selectedData - 1).toString())
+                        .transition()
+                        .attr("font-size", 12);
+
+                    d3.select("#streamer_text_" + (selectedData + 1).toString())
+                        .transition()
+                        .attr("font-size", 12);
+
+                    this.parentElement.appendChild(this);
+                })
+                .on("mouseout", function(event, selectedData) {
+                    d3.select(this)
+                        .transition()
+                        .attr("font-size", 9);
+
+                    d3.select("#streamer_text_" + (selectedData - 1).toString())
+                        .transition()
+                        .attr("font-size", 9);
+
+                    d3.select("#streamer_text_" + (selectedData + 1).toString())
+                        .transition()
+                        .attr("font-size", 9);
+                });
+
+        var data = ['Stream Count', 'Avg Viewers'];
+        
+        var select = d3.select('#encoding-selector-container')
+            .append('select')
+                .attr('class','select')
+            .on('change',onchange);
+        
+        var options = select
+            .selectAll('option')
+            .data(data).enter()
+            .append('option')
+                .text(function (d) { return d; });
+        
+        function onchange() {
+            selectValue = d3.select('select').property('value')
+            const new_color = d3.scaleSequential(d3.interpolateBlues).domain([0, 60]);
+            d3.select('#gantt-chart-svg')
+                .selectAll('rect')
+                .attr("fill", d => new_color(d.count));
+        };
+    }
+);
+
 
 function getImageUrl(streamer) {
     return "img/" + streamer["Rank"] + "_"+ streamer["Channel URL"] + ".jpeg";
@@ -73,3 +184,37 @@ function markAsSelected(elem) {
             .style("opacity", "0.5")
             .style("background-color", "blueviolet");
 }
+
+function getStreamsData(raw_data) {
+    var arr = [];
+    const months = {'Jan': 0, 'Feb': 1, 'Mar': 2, 'Apr': 3, 'May': 4, 'Jun': 5, 'Jul': 6, 'Aug': 7, 'Oct': 8, 'Sep': 9, 'Nov': 10, 'Dec': 11};
+    
+    for (var idx in raw_data) {
+        const curr_obj = raw_data[idx];
+        if (curr_obj == null || curr_obj['rank'] == null) { continue; }
+        
+        const raw_start_date = curr_obj['start_month'].split('-');
+        
+        arr = arr.concat({
+        rank: parseInt(curr_obj['rank']),
+        name: curr_obj['streamer'],
+        start: new Date('20' + raw_start_date[1], months[raw_start_date[0]]),
+        avg_viewers: parseFloat(curr_obj['avg_avg_viewers']),
+        count: parseInt(curr_obj['count']),
+        });
+    }
+    
+    return arr;
+}
+
+function dateDiff(date_1, date_2) {
+    first_date = date_1 < date_2 ? date_1 : date_2;
+    second_date = date_1 < date_2 ? date_2 : date_1;
+
+    var months = (second_date.getFullYear() - first_date.getFullYear())*12;
+    months -= first_date.getMonth();
+    months += second_date.getMonth();
+    return months;
+
+    return Math.round(Math.abs((date_1 - date_2)/(1000 * 60 * 60 * 24)));
+  }
